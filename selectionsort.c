c59bd900525ddf37031f65a15e491dc7ef70a19f
asheshiqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-05-16 14:59:12 +0600

    Add files via upload

---
+++ selectionsort.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int i, j, n=10, min, tmp;
+    int a[10];
+    for(i=0;i<n;i++)
+    {
+        a[i] = rand()%500;
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d ", a[i]);
+}
+////////////////////////////////////////
+    for(i=0;i<n;i++)
+    {
+        min = i;
+        for(j=i+1;j<n;j++)
+        {
+            if(a[j]<a[min])
+            {
+                min = j;
+            }
+        }
+        tmp = a[i];
+        a[i] = a[min];
+        a[min] = tmp;
+    }
+////////////////////////////////////////
+    printf("\n");
+    for(i=0;i<n;i++)
+    {
+        /* if(i!=n-1 && a[i]>a[i+1])
+        {
+            printf("WRONG!!!");
+            return;
+        } */
+        printf("%d ", a[i]);
+    }
+}
