5fca855ed5ef4b1e9890cb1532356f5c198b124d
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-05-18 00:18:44 +0600

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -15,3 +15,15 @@ int main()
     scanf("%d",&n);
     printf("%d",f(n));
 }
+
+/* without recursion
+int main()
+{
+    int n, i, f = 1;
+    scanf("%d", &n);
+    for(i=n; i>=1; i--)
+    {
+        f = f * i;
+    }
+    printf("%d", f);
+}*/
