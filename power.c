fac0ec1b007785a0b65fc7460176c290069a21a0
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-05-18 00:54:44 +0600

    Add files via upload

---
+++ power.c
@@ -0,0 +1,27 @@
+// Power
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+
+{
+    
+	int a, n, i, r = 1;
+
+	scanf("%d %d", &a, &n);
+
+    
+    for(i=1; i<=n; i++)
+
+	{
+		r *= a;
+	}
+
+    
+    printf("%d", r);
+
+}
