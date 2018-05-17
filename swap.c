510655ed6339fa70677001f90bb34895f9bf22f6
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-05-17 19:42:55 +0600

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,16 @@
+// Swapping two numbers without third variable
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("a = %d, b = %d \n", a, b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("a = %d, b = %d", a, b);
+}
+
+
