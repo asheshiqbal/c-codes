7b963385d5718a01a5bf11638c4c049c39232409
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-05-18 01:30:42 +0600

    Add files via upload

---
+++ sinex.c
@@ -0,0 +1,28 @@
+// sine (x)
+
+
+
+
+#include <stdio.h>
+
+
+
+int main()
+
+{
+	int i, n = 4, deg;
+	float r, x, term ;
+	scanf("%d", &deg);
+	x = deg*3.1416/180;
+	
+	term = x;
+	r = x;
+	
+	for(i=1; i<=n; i++)
+	{
+		term = term * (-1)* x*x/((i+1)*(i+2));
+		r += term;
+	}
+
+	printf("%f", r);
+}
