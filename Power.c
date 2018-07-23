db45de6c3eedae7e82edc0ab5dc78412261754e8
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-07-23 23:30:04 +0600

    Update and rename power.c to Power.c

--- Power.c
+++ Power.c
@@ -1,27 +1,16 @@
 // Power
 
-
-
 #include <stdio.h>
 
-
-
 int main()
-
 {
-    
 	int a, n, i, r = 1;
-
 	scanf("%d %d", &a, &n);
-
-    
-    for(i=1; i<=n; i++)
-
+	
+	for(i=1; i<=n; i++)
 	{
 		r *= a;
 	}
-
-    
-    printf("%d", r);
-
+	
+	printf("%d", r);
 }
