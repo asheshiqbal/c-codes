352af3021eb245c1d9cb07ad42f953b373cd7500
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-07-23 23:31:18 +0600

    Update and rename convergence_sum.c to Convergence.c

--- Convergence.c
+++ Convergence.c
@@ -4,13 +4,13 @@
 
 int main()
 {
-    int s=0,n=1234;
+    int s=0, n=1234;
     while(n>0)
     {
         while(n>0)
         {
-            s+=n%10;
-            n=n/10;
+            s += n%10;
+            n = n/10;
         }
         printf("%d ",s);
         if(s>9)
