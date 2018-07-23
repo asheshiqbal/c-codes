ada7daacb0e8833954e02f0f7d2d8b2e8dbd8658
Ashesh Iqbal <33754010+asheshiqbal@users.noreply.github.com>
2018-07-23 23:38:39 +0600

    Update and rename max-min.c to Find Max-Min.c

--- Find Max-Min.c
+++ Find Max-Min.c
@@ -1,18 +1,22 @@
-// Max Min
+// Find the Maximum and the Minimum from the input numbers.
 
 #include<stdio.h>
 
 int main()
 {
-    int ip,i,n,max=-100000,min=100000;
+    int ip, i, n, max=-100000, min=100000;
+    
     scanf("%d",&n);
+    
     for(i=0;i<n;i++)
     {
         scanf("%d",&ip);
+        
         if(ip>max)
-        max=ip;
+            max=ip;
         if(ip<min)
-        min=ip;
+            min=ip;
     }
+    
     printf("%d %d",max,min);
 }
